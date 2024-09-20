7f51a87ff0b90d709e5c47721fab28633ab60082
ciro-s <85528837+ciro-s@users.noreply.github.com>
2024-09-20 12:18:20 -0300

    Funcionalidad de eliminar y restaurar usuarios

--- StyleSys/Forms/Usuarios/ListaUsuarios.cs
+++ StyleSys/Forms/Usuarios/ListaUsuarios.cs
@@ -38,7 +38,15 @@ namespace StyleSys.Forms.Usuarios
 
             for (int i = 0; i < usuarios.Count; i++)
             {
-                dgvUsuarios.Rows.Add(i + 1, usuarios[i].id_usuario, usuarios[i].us_nombre, usuarios[i].us_apellido, usuarios[i].us_email, usuarios[i].us_fechaNacimiento, usuarios[i].us_telefono);
+                //Escribe el estado del usuario
+                string estado = "HABILITADO";
+                if (!usuarios[i].us_estado)
+                {
+                    estado = "DESHABILITADO";
+                }
+
+                //Agrega el registro al grid
+                dgvUsuarios.Rows.Add(i + 1, usuarios[i].id_usuario, usuarios[i].us_nombre, usuarios[i].us_apellido, usuarios[i].us_email, usuarios[i].us_fechaNacimiento, usuarios[i].us_telefono, estado);
             }
         }
 
@@ -46,13 +54,14 @@ namespace StyleSys.Forms.Usuarios
         private void dgvUsuarios_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             string col = dgvUsuarios.Columns[e.ColumnIndex].Name;
+            var id = int.Parse(dgvUsuarios[1, e.RowIndex].Value.ToString());
+            Usuario user = _context.Usuarios.Find(id);
 
+            //CLICK EN BOTON EDITAR
             if (col == "editar")
             {
                 ModuloUsuarios modulo = new ModuloUsuarios(this);
-                var id = int.Parse(dgvUsuarios[1, e.RowIndex].Value.ToString());
 
-                Usuario user = _context.Usuarios.Find(id);
                 if (user != null)
                 {
                     //Deshabilitar el botón de guardar
@@ -74,28 +83,65 @@ namespace StyleSys.Forms.Usuarios
                     MessageBox.Show("Ocurrió un error recuperando al usuario seleccionado.", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
-            else if (col == "Eliminar")
+            else if (col == "Eliminar") //CLICK EN BOTÓN ELIMINAR
             {
-                var id = int.Parse(dgvUsuarios[1, e.RowIndex].Value.ToString());
-                Usuario user = _context.Usuarios.Where(u => u.id_usuario == id).FirstOrDefault();
-                if (user != null && MessageBox.Show("¿Está seguro de que quiere eliminar este usuario?\n" + user.us_nombre + " " + user.us_apellido, "Eliminar", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                //Verificar que existe el usuario y está habilitado
+                if (user != null && user.us_estado)
                 {
-                    user.us_estado = false;
-                    _context.SaveChanges();
+                    if (MessageBox.Show("¿Está seguro de que quiere eliminar este usuario?\n" + user.us_nombre + " " + user.us_apellido, "Eliminar", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        user.us_estado = false; //deshabilita al usuario
+                        _context.SaveChanges(); //guarda la actualización
+                    }
                 }
-                bindDGView(_context.Usuarios.Where(u => u.us_estado == true).ToList());
+                else if (!user.us_estado)
+                {
+                    MessageBox.Show("Este usuario ya está deshabilitado.", "No se pudo eliminar", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                bindDGView(_context.Usuarios.ToList());
+                checkEliminados.Checked = true;
+            }
+            else if (col == "restaurar")
+            {
+                //Verificar que existe el usuario y está deshabilitado
+                if (user != null && !user.us_estado)
+                {
+                    if (MessageBox.Show("¿Está seguro de que quiere RESTAURAR este usuario?\n" + user.us_nombre + " " + user.us_apellido, "Restaurar", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        user.us_estado = true; //habilita al usuario
+                        _context.SaveChanges(); //guarda la actualización
+                    }
+                }
+                else if (user.us_estado)
+                {
+                    MessageBox.Show("Este usuario ya está habilitado.", "No se pudo restaurar", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                bindDGView(_context.Usuarios.ToList());//Muestra todos los usuarios en el grid
+                checkEliminados.Checked = true;
             }
         }
 
+        /*
+         * Envia una lista de usuarios al DataGridView según si se requieren los eliminados o no
+         */
         private void checkEliminados_CheckedChanged(object sender, EventArgs e)
         {
+            //Si el checkbox no está seleccionado...
             if (!checkEliminados.Checked)
             {
+                //Se muestran solo los usuarios habilitados
                 bindDGView(_context.Usuarios.Where(u => u.us_estado == true).ToList());
+                dgvUsuarios.Columns[7].Visible = false; //esconde columna de estado
+                dgvUsuarios.Columns[10].Visible = false; //esconde columna de restaurar
             }
-            else
+            else //Si se selecciona el checkbox
             {
+                //Muestra TODOS los usuarios
                 bindDGView(_context.Usuarios.ToList());
+                dgvUsuarios.Columns[7].Visible = true; //Muestra columna de estado
+                dgvUsuarios.Columns[10].Visible = true; //Muestra columna de restaurar
             }
         }
     }
